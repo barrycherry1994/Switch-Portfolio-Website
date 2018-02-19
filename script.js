9f26c3b2cfa6cf8c2a93f447d0ec567d7f8f7940
Barry Cherry <barrycherry1994@gmail.com>
2018-02-19 11:18:39 -0500

    Switch Project

---
+++ script.js
@@ -0,0 +1,28 @@
+    function clickOne() {
+    document.getElementById("btn-1").style.background = "green";
+    document.getElementById("btn-1").style.color = "white";
+    document.getElementById("btn-2").style.background = "white";
+    document.getElementById("btn-2").style.color = "green";
+    document.getElementById("btn-3").style.background = "white";
+    document.getElementById("btn-3").style.color = "green";
+  
+}
+
+function clickTwo() {
+    document.getElementById("btn-2").style.color = "white";
+    document.getElementById("btn-2").style.background = "green";
+    document.getElementById("btn-1").style.background = "white";
+    document.getElementById("btn-1").style.color = "green";
+    document.getElementById("btn-3").style.background = "white";
+    document.getElementById("btn-3").style.color = "green";
+   
+}
+
+function clickThree() {
+    document.getElementById("btn-3").style.color = "white";
+    document.getElementById("btn-3").style.background = "green";    
+    document.getElementById("btn-1").style.background = "white";
+    document.getElementById("btn-1").style.color = "green";
+    document.getElementById("btn-2").style.background = "white";
+    document.getElementById("btn-2").style.color = "green";
+}
